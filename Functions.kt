c147c5ba3cffefdf03d34a9dd78e4c5080d08ad9
saiman999 <mzaf04@gmail.com>
2020-08-08 20:31:13 +0700

    Seventh commit

---
+++ Functions.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val user = setUser("Zaki", "Gaming", 16)
+}
+
+fun setUser(name: String, hobby: String, age: Int)  {
+    println("My name is $name, I like $hobby, and I'm $age years old.")
+}
