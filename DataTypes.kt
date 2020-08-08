7d0685077560933a5416cdab760d681fb5a99ca5
saiman999 <mzaf04@gmail.com>
2020-08-08 20:28:10 +0700

    Fifth commit

---
+++ DataTypes.kt
@@ -0,0 +1,10 @@
+fun main () {
+    val firstword = "SMK "
+    val middleword = "Telkom "
+    val lastword = "Purwokerto "
+    println(firstword+middleword+lastword)
+
+    val valueword = "Luas Persegi Panjang "
+    val value = 86
+    println(valueword+value)
+}
