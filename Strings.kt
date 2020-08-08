e212adbb6368f8547e564a72deda15f4686973c3
saiman999 <mzaf04@gmail.com>
2020-08-08 20:32:16 +0700

    Last commit in this day

---
+++ Strings.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val wordOne = "Modern"
+    val wordTwo= "Concise"
+    val wordThree = "Pragmatic"
+    val wordFour = "Safe"
+
+    println("Kotlin is $wordOne")
+    println("Kotlin is $wordTwo")
+    println("Kotlin is $wordThree")
+    println("Kotlin is $wordFour")
+}
