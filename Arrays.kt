16c031e709e3e6a9da2a190d6a2da9e34cf3bfdf
saiman999 <mzaf04@gmail.com>
2020-08-08 20:25:15 +0700

    Third commit

---
+++ Arrays.kt
@@ -0,0 +1,11 @@
+fun main (){
+    val sekolah = "SMK Telkom Purwokerto berdiri pada tanggal "
+    val bulan = " Januari"
+    val kata = ".That’s true bro."
+    val intArrays = intArrayOf(10,20,30,1992,1993,1994)
+    println(sekolah)
+    print(intArrays[2])
+    println(bulan)
+    print(intArrays[4])
+    println(kata)
+}
